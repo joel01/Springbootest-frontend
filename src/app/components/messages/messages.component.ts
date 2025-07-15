c7d64061abb75309bd906123109a261b5e0c640d
joel01 <jpolickm@hotmail.com>
2025-07-14 20:34:53 -0500

    componente messages y models

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { ContactMessage } from '../../models/contact-message.model'; // Si no tienes el modelo, ahora te lo paso
 
 @Component({
   selector: 'app-messages',
@@ -6,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit {
+  messages: ContactMessage[] = [];
 
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.getMessages().subscribe(
+      (data) => {
+        this.messages = data;
+      },
+      (error) => {
+        console.error('Error al obtener mensajes', error);
+      }
+    );
+  }
 }

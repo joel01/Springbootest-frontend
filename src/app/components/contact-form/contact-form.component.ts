4f522adb78d9f120baf1de1cb23537a06e3ce5b8
joel01 <jpolickm@hotmail.com>
2025-07-14 20:17:33 -0500

    llamar al endpoint con ApiService y envíe el mensaje

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
+  name: string = '';
+  email: string = '';
+  message: string = '';
+  response: string = '';
 
+  constructor(private apiService: ApiService) {}
+
+  sendMessage() {
+    const payload = {
+      name: this.name,
+      email: this.email,
+      message: this.message
+    };
+
+    this.apiService.sendContactMessage(payload).subscribe({
+      next: (res) => {
+        this.response = res;
+        this.name = '';
+        this.email = '';
+        this.message = '';
+      },
+      error: (err) => {
+        this.response = 'Error al enviar el mensaje.';
+        console.error(err);
+      }
+    });
+  }
 }
